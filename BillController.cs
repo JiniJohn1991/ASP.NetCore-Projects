430255580cb6514f0acf8433a1a6ac2f791d2042
JiniJohn1991 <32994494+JiniJohn1991@users.noreply.github.com>
2019-01-17 17:29:08 -0800

    Add files via upload

---
+++ BillController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HomeManagementApplicationMain.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HomeManagementApplicationMain.Controllers
+{
+    [Route("HomeManagementApplicationMain/[controller]")]
+    public class BillController : Controller
+    {
+        private HMDBContext context;
+        public IConfiguration Configuration { get; set; }
+
+        public BillController(HMDBContext dbContext)
+        {
+            context = dbContext;
+        }
+        public IActionResult Bill()
+        {
+
+            BillMaster billDetails = new BillMaster();
+            var billDdl = context.BillMaster.Select(y => new { y.BillId, y.BillName });
+            List<SelectListItem> BillList = new List<SelectListItem>();
+            foreach (var item in billDdl)
+            {
+                BillList.Add(new SelectListItem { Text = item.BillName, Value = item.BillId.ToString() });
+            }
+            billDetails.BillList = BillList;
+            return View(billDetails);
+        }
+        [HttpPost]
+        public IActionResult Bill(BillDetails billDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                BillDetails details = new BillDetails();
+                details.Amount = billDetails.Amount;
+                details.Date = billDetails.Date;
+                details.Store = billDetails.Store;
+                context.Add<BillDetails>(billDetails);
+                context.SaveChanges();
+            }
+            return View();
+        }
+    }
+}
---
+++ UserLoginController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HomeManagementApplicationMain.Models;
+
+namespace HomeManagementApplicationMain.Controllers
+{
+    [Route("HomeManagementApplicationMain/[controller]")]
+    public class UserLoginController : Controller
+    {
+        // GET: Login
+        private HMDBContext context;
+
+        public UserLoginController(HMDBContext dbContext)
+        {
+            context = dbContext;
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(UserLogin loginDetails)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                if (loginDetails != null && !string.IsNullOrEmpty(loginDetails.UserName) &&
+                    !string.IsNullOrEmpty(loginDetails.Password))
+                {
+                    var userNameInDB = context.UserLogin.Where(x => x.UserName.Equals(loginDetails.UserName)).FirstOrDefault();
+                    if (userNameInDB != null)
+                    {
+                        if (userNameInDB.Password.Equals(loginDetails.Password))
+                        {
+                            RedirectToAction("Main");
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("password", "Incorrect password");
+                        }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("username", "Incorrect username");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("errorMessage", "Please enter username and password to login");
+                }
+            }
+            return View(loginDetails);
+        }
+    }
+}
